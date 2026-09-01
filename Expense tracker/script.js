6fe503e39d5ee76400f1a6d3e2f063828a7e29f4
Akshay bavaskar <bavaskar511@gmail.com>
2026-09-01 19:40:15 +0530

    Click function

--- Expense tracker/script.js
+++ Expense tracker/script.js
@@ -2,3 +2,6 @@ console.log("Javascript is connected");
 let entryDesc=document.querySelector("entry-desc");
 let entryAmount=document.querySelector("entry-amount");
 
+entryDesc.addEventListener("click", function() {
+    console.log("Description clicked");
+});
