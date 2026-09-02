8178d3f79e6990f3032a2e1c2772c02cc2b593a6
Akshay bavaskar <bavaskar511@gmail.com>
2026-09-02 22:20:43 +0530

    Added btn that takes values when we fill

--- Expense tracker/script.js
+++ Expense tracker/script.js
@@ -1,10 +1,23 @@
 console.log("Javascript is connected");
-let date=document.querySelector("#date");
+//taking date value as current date
+let entryDate=document.querySelector("#entry-date");
+entryDate.value=new Date().toISOString().split("T")[0];
+
+//taking input values
 let entryDesc=document.querySelector("#entry-desc");
 let entryAmount=document.querySelector("#entry-amount");
-let addBtn=document.querySelector("#add-btn");
-    
+let entryCategory=document.querySelector("#entry-category");
 
+
+//taking add button when we click on it we will add the entry to the list
+let addBtn=document.querySelector(".add-btn");
+    
+//when we click on add button we will add the entry to the list
 addBtn.addEventListener("click", function() {
     console.log("Add button clicked");
+    let desc=entryDesc.value;
+    let amount=Number(entryAmount.value);
+    let category=entryCategory.value;
+    let date=entryDate.value;
+    console.log(desc, amount, category, date);
 });
