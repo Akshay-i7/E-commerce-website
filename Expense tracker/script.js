dcd8312867a165913a39b161b21d195fad04da0b
Akshay bavaskar <bavaskar511@gmail.com>
2026-09-02 22:34:46 +0530

    Done adding value taker

--- Expense tracker/script.js
+++ Expense tracker/script.js
@@ -7,11 +7,14 @@ entryDate.value=new Date().toISOString().split("T")[0];
 let entryDesc=document.querySelector("#entry-desc");
 let entryAmount=document.querySelector("#entry-amount");
 let entryCategory=document.querySelector("#entry-category");
+let entriesList=document.querySelector("#entries-list");
+
 
 
 //taking add button when we click on it we will add the entry to the list
 let addBtn=document.querySelector(".add-btn");
-    
+
+
 //when we click on add button we will add the entry to the list
 addBtn.addEventListener("click", function() {
     console.log("Add button clicked");
@@ -20,4 +23,16 @@ addBtn.addEventListener("click", function() {
     let category=entryCategory.value;
     let date=entryDate.value;
     console.log(desc, amount, category, date);
+    let row=document.createElement("tr");
+    let dateCell=document.createElement("td");
+    let descCell=document.createElement("td");
+    let categoryCell=document.createElement("td");
+    let amountCell=document.createElement("td");
+    dateCell.textContent=date;
+    descCell.textContent=desc;
+    categoryCell.textContent=category;
+    amountCell.textContent=amount;
+    row.append(dateCell,descCell,categoryCell,amountCell);
+    entriesList.appendChild(row);
+    
 });
